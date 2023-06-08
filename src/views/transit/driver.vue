314e2c634426f567492393e91296315ee598be43
nan <1013383995@qq.com>
2023-06-08 14:45:41 +0800

    基础

---
+++ src/views/transit/driver.vue
@@ -0,0 +1,28 @@
+<!-- 车辆管理 -->
+<template>
+  <div class="dashboard-container driver customer-list-box">
+
+  </div>
+</template>
+<script>
+export default {
+  name: 'Courses'
+}
+</script>
+<style rel="stylesheet/scss" lang="scss" scoped>
+.alert {
+  margin: 10px 0px;
+}
+.pagination {
+  margin-top: 40px;
+}
+// 下拉框
+::v-deep .el-select {
+  width: 100%;
+}
+
+::v-deep .el-card{
+  overflow: initial;
+}
+</style>
+
---
+++ src/views/transit/workArrange-setting.vue
@@ -0,0 +1,69 @@
+<!-- 排班设置页 -->
+<template>
+  <div class="dashboard-container waybill-list customer-list-box">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+::v-deep .table-card-box {
+  .el-card__body {
+    padding-bottom: 0px;
+  }
+}
+::v-deep .pagination{
+  padding-bottom: 30px;
+}
+.tab-box {
+  color: #20232a;
+  ::v-deep .el-tabs__item.is-active {
+    color: #20232a;
+    font-size: 16px;
+    font-weight: bold;
+  }
+  ::v-deep .el-form-item__content {
+    height: 40px;
+  }
+  ::v-deep .el-tabs__item {
+    font-size: 14px;
+  }
+  ::v-deep .el-tabs__item:hover {
+    color: #20232a;
+    font-size: 16px;
+    font-weight: bold;
+    cursor: pointer;
+  }
+  ::v-deep .el-tabs__active-bar {
+    background-color: #e15536;
+  }
+  ::v-deep .tips {
+    margin-left: 130px;
+    margin-bottom: 14px;
+    color: #bac0cd;
+  }
+  .el-checkbox {
+    margin-right: 12px;
+  }
+  ::v-deep .workDay {
+    .el-form-item__error {
+      position: relative;
+    }
+  }
+}
+::v-deep .el-dialog__body {
+  padding-top: 30px !important;
+  .el-tabs__header {
+    padding-bottom: 25px;
+  }
+}
+::v-deep .el-tabs__nav,
+.is-top {
+  transform: translateX(30px) !important;
+}
+</style>
